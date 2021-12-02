ea26dccf600606dfa927211fa490fa25ee275ebd
Robbie Hammond <robbiehammond3@gmail.com>
2021-12-02 10:46:35 -0500

    part 2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let filename = "src/input.txt";
     let file = File::open(filename).unwrap();
     let mut reader = BufReader::new(file);
-    println!("{}", day2(reader))
+    println!("{}", day2(reader, "2"))
 }
 
 fn day1(mut reader: BufReader<File>) -> i32 {
@@ -25,20 +25,38 @@ fn day1(mut reader: BufReader<File>) -> i32 {
 }
 
 
-fn day2(mut reader: BufReader<File>) -> i32 {
-    let mut depth = 0;
-    let mut horz = 0;
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let pair = line.split(" ").collect::<Vec<&str>>();
+fn day2(mut reader: BufReader<File>, part: &str) -> i32 {
+    if part == "1" {
+        let mut depth = 0;
+        let mut horz = 0;
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let pair = line.split(" ").collect::<Vec<&str>>();
 
-        let num = pair[1].parse::<i32>().unwrap();
-        if pair[0] == "forward" {
-            horz += num
+            let num = pair[1].parse::<i32>().unwrap();
+            if pair[0] == "forward" {
+                horz += num
+            } else {
+                depth = if pair[0] == "up" { depth - num } else { depth + num };
+            }
         }
-        else {
-            depth = if pair[0] == "up" { depth - num } else {depth + num };
-        }
+        depth * horz
     }
-    depth * horz
+    else {
+        let mut depth = 0;
+        let mut horz = 0;
+        let mut aim = 0;
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let pair = line.split(" ").collect::<Vec<&str>>();
+            let num = pair[1].parse::<i32>().unwrap();
+            if pair[0] == "forward" {
+                horz += num;
+                depth += aim * num;
+            } else {
+                aim = if pair[0] == "up" { aim - num } else { aim + num };
+            }
+        }
+        depth * horz
+    }
 }
